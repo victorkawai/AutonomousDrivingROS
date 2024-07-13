f8925e1a17934197a38f931daf61d62a3e59cb3c
Tibor <tibor.von.gencsy@gmail.com>
2024-07-13 13:04:20 +0200

    Re-wrote path tracker to hopefully correctly work now.

---
+++ ros_ws/src/path_tracker/src/path_tracker_node.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import rospy
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+class PathCreator:
+    def __init__(self):
+        # Initialize the ROS node
+        rospy.init_node('path_tracker', anonymous=True)
+
+        # Subscriber to the pose topic
+        self.pose_sub = rospy.Subscriber('/unity_ros/OurCar/Sensors/IMU/twist', PoseStamped, self.pose_callback)
+
+        # Publisher for the path topic
+        self.path_pub = rospy.Publisher('/car_path', Path, queue_size=10)
+
+        # Initialize the Path message
+        self.path = Path()
+        self.path.header.frame_id = "world"  # Use the appropriate frame ID
+
+    def pose_callback(self, pose_msg):
+        # Append the new pose to the path
+        self.path.poses.append(pose_msg)
+
+        # Update the header timestamp
+        self.path.header.stamp = rospy.Time.now()
+
+        # Publish the path
+        self.path_pub.publish(self.path)
+
+    def run(self):
+        # Keep the node running
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        path_creator = PathCreator()
+        path_creator.run()
+    except rospy.ROSInterruptException:
+        pass
